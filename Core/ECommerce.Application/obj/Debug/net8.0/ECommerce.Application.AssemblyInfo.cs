43a46c1a26eee80008909ddd028518668f128634
Siya <siajabbarova@gmail.com>
2024-05-25 16:31:54 +0400

    second

--- Core/ECommerce.Application/obj/Debug/net8.0/ECommerce.Application.AssemblyInfo.cs
+++ Core/ECommerce.Application/obj/Debug/net8.0/ECommerce.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ECommerce.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+898aa75a0f550218cf02fcb96e0cfffe90c50e64")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f59361be4264d5965776cb697187da2c8bd0da7c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ECommerce.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ECommerce.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
